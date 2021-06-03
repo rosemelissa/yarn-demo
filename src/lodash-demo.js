45880f3ff24d9fe62c3645fbedc6e81a5275b188
Richard Ng <richardcrng@gmail.com>
2021-06-03 21:53:16 +0100

    Import lodash as _ for docs consistency

---
+++ src/lodash-demo.js
@@ -0,0 +1,6 @@
+const _ = require('lodash');
+
+const arr = [1, 2, 3];
+
+console.log(_.head(arr), "is the head of", arr);
+console.log(_.tail(arr), "is the tail of", arr);
--- src/script.js
+++
@@ -1,6 +0,0 @@
-const { head, tail } = require('lodash');
-
-const arr = [1, 2, 3];
-
-console.log(head(arr), "is the head of", arr);
-console.log(tail(arr), "is the tail of", arr);
