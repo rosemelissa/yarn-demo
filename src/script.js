14a9ee3689f282f6a35b3c92b2d8334ce20b302f
Richard Ng <richardcrng@gmail.com>
2021-06-03 21:03:36 +0100

    Change to use head and tail from lodash for simplicity

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
-const { isEqual } = require('lodash');
+const { head, tail } = require('lodash');
 
-const arrOne = [1, 2, 3];
-const arrTwo = [1, 2, 3];
+const arr = [1, 2, 3];
 
-console.log("do arrOne and arrTwo have strict equality (referential identity) (===)?", arrOne === arrTwo);
-console.log("do arrOne and arrTwo have deep value equality?", isEqual(arrOne, arrTwo));
+console.log(head(arr), "is the head of", arr);
+console.log(tail(arr), "is the tail of", arr);
